0a7e978f28c164e22a6c3e852edc67a3588d505d
„Mikołaj <mikolaj.zendarski@student.wat.edu.pl>
2021-01-11 10:03:53 +0100

    Wykonano zadanie 3.3.1 – tekst statyczny

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -1,39 +1,16 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 
-//Inicjalizacja obiektu klasy LiquidCrystal:
+//Inicjalizacja obiektu klasy LiquidCrystal: RS, E, DB4, DB5, DB6, DB7,
 LiquidCrystal lcd(12,11,5,4,3,2);
-char tablica[] = "Mikolaj";
-int tablen = 0;
-
-void lcdClear(void);
 
 void setup() {
-  lcd.begin(16,2); //Ustawienie liczby kolu
-  lcd.print("Mikolaj Zendarski");
-  tablen = sizeof(tablica);
-  delay(2000);
+  lcd.begin(16,2);  // ustawienie liczby kolumn i wierszy
+  lcd.print("Zendarski"); // Komunikat powitalny
 }
 
 void loop() {
-  for (int i = 0; i < (18- tablen); i++) {
-    lcdClear();
-    lcd.setCursor(i, 0);
-    lcd.print(tablica);
-    delay(1000);
-  }
-
-  for (int i = ( 17- tablen); i >= 0; i--) {
-  lcdClear();
-  lcd.setCursor(i, 1);
-  lcd.print(tablica);
-  delay(1000);
- }
-}
-
-void lcdClear (void)  {
-  lcd.setCursor(0, 0);
-  lcd. print ("                 ");
-  lcd.setCursor(0, 1);
-  lcd. print ("                 ");
+  lcd.setCursor(0, 1); // Przesunięcie kursora do początku drugiego wiersza
+  lcd.print(millis() / 1000); / Wyświetlenie liczby sekund od uruchomienia
+  delay(500);
 }
