41f8a86ca317cd700c65c6f70054550638b97fc6
„Mikołaj <mikolaj.zendarski@student.wat.edu.pl>
2021-01-11 10:09:10 +0100

    Wykonano zadanie dodatkowe 3.3.3 – ustawianie daty, operacje matematyczne

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -1,16 +1,27 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
+#include <stdio.h>
 
 //Inicjalizacja obiektu klasy LiquidCrystal: RS, E, DB4, DB5, DB6, DB7,
 LiquidCrystal lcd(12,11,5,4,3,2);
+char rxBuff [32];
+int ValueA = 0, ValueB =0;
+char operation[16];
 
 void setup() {
-  lcd.begin(16,2);  // ustawienie liczby kolumn i wierszy
-  lcd.print("Zendarski"); // Komunikat powitalny
+  lcd.begin(16, 2 ); //Ustawienie liczby kolumn i wierszy
+  lcd.print(" Mikolaj Zendarski");
+  Serial.begin(9600);
+  Serial.setTimeout(3000);
+  delay (2000);
 }
 
 void loop() {
-  lcd.setCursor(0, 1); // Przesunięcie kursora do początku drugiego wiersza
-  lcd.print(millis() / 1000); / Wyświetlenie liczby sekund od uruchomienia
-  delay(500);
+  Serial.readBytesUntil('\r', rxBuff, sizeof(rxBuff) - 1);
+  //Odczyt wartości i ich konwersja
+  lcd.setCursor(0, 0); // W pierwszym wierszu jest wpisany tekst
+  lcd.print(rxBuff);
+  lcd.setCursor(0,1);
+  lcd.print("=");
+  //Wykonanie obliczeń i wyświetlenie wyniku
 }
